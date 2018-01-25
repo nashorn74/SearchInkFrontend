637fcb5cf544d423c847d4c3eef53cf5987cccbb
Jeong kum ho <nashorn74@gmail.com>
2018-01-25 21:01:02 +0900

    Add SockJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient,HttpParams,HttpHeaders } from '@angular/common/http';
+import Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +12,40 @@ import { HttpClient,HttpParams,HttpHeaders } from '@angular/common/http';
 export class AppComponent {
   title = 'Schedule Tasks';
 
-  constructor(private http: HttpClient){
+  private serverUrl = 'http://127.0.0.1:8080/gs-guide-websocket';
+  private stompClient;
+
+  initializeWebSocketConnection(){
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/topic/greetings", (message) => {
+        if(message.body) {
+          //console.log(message.body);
+          if (message.body instanceof Array){
+            console.log('recieve data:'+message.body.length);
+            this.tasks = message.body;
+            if (message.body.length > 0) {
+              this.task = message.body[0];
+            }
+          }
+        }
+      });
+    });
   }
 
-  public task:{};
-  public tasks:[]; 
+  sendMessage(message){
+    this.stompClient.send("/app/hello" , {}, message);
+    $('#input').val('');
+  }
+
+  constructor(private http: HttpClient){
+    this.initializeWebSocketConnection();
+  }
+
+  public task = {};
+  public tasks = []; 
   ngOnInit(): void {
     this.loadTask();
   }
@@ -21,10 +53,13 @@ export class AppComponent {
   loadTask() {
   	this.http.get('http://127.0.0.1:8080/tasks').subscribe(data => {
       console.log(data);
-      this.tasks = data;
-      if (data.length > 0) {
-      	this.task = data[0];
+      if (data instanceof Array){
+        this.tasks = data;
+        if (data.length > 0) {
+          this.task = data[0];
+        }
       }
+      
     });
   }
 
